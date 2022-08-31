e98ec7d046b6a0a9867ccf452763968d3f9c1fb7
Dendi <dendiaa@students.amikom.ac.id>
2022-08-31 13:59:39 +0700

    feat: weekly-task revision

--- weekly-task/task-1.go
+++ weekly-task/task-1.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Barang struct {
 	name  string
@@ -20,16 +22,25 @@ func main() {
 		{"Benih Cupang", 10000},
 		{"Probiotik B", 10000},
 	}
+
+	fmt.Println("Item yang memiliki harga sama: ")
 	fmt.Println(HargaSama(barang[:], 10000))
+
+	fmt.Println("Memanggil harga termurah dan termahal: ")
 	fmt.Println(MurahMahal(barang[:]))
+
+	fmt.Println("Total barang yang bisa dibeli: ")
+	//fmt.Println(TotalP("Belum bisa"))
 }
 
+// func TotalP(harga int) ([]Barang, int) {
+
+// }
+
 func MurahMahal(arr []Barang) (Barang, Barang) {
-	Panjang := len(arr)
-	for i := 0; i < Panjang; i++ {
-		j := i + 1
-		var temp Barang
-		for j < Panjang {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			var temp Barang
 			if arr[i].harga > arr[j].harga {
 				temp = arr[i]
 				arr[i] = arr[j]
@@ -37,7 +48,7 @@ func MurahMahal(arr []Barang) (Barang, Barang) {
 			}
 		}
 	}
-	return arr[0], arr[Panjang-1]
+	return arr[0], arr[len(arr)-1]
 }
 
 func HargaSama(arr []Barang, harga int) []string {
